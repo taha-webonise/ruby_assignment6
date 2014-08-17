6ddecc274c378164f068f877a62277959697fa12
Taha Husain <taha.husain@weboniselab.com>
2014-08-17 06:03:51 +0530

    Finish Part two : Assignment 6

--- meta_programming.rb
+++ meta_programming.rb
@@ -2,35 +2,36 @@
 require 'csv'
 require 'active_support/core_ext/string'
 
-file_name = "users.csv"
-header = CSV.read(file_name,"r")[0]
-content = CSV.read(file_name, "r")
-content.shift
+binding = IRB.conf[:MAIN_CONTEXT].workspace.binding
+
+# file_name = "users.csv"
+print "Please enter csv file name: "
+file_name = $stdin.gets.chomp
+contents = CSV.read(file_name, "r")
+header = contents.shift
 header_s = []
 header.each do |h|
   header_s.push(h.downcase.tr(" ", "_"))
 end
 
-class NewClass
-  def self.dynamic_create(class_name, *cols)
-    instance = Class.new do
-      cols.each do |col|
-      	attr_accessor col.intern
-      end
+def create(class_name, *vars)
+  instance = Class.new do
+    vars.each do |var|
+    	attr_accessor var.intern
     end
-    Object.const_set class_name, instance
   end
+  Object.const_set class_name, instance
 end
 
 class_name = file_name.split(".")[0].capitalize.singularize
-NewClass.dynamic_create(class_name, *header_s)
+create(class_name, *header_s)
 
 obj_array = []
-
-content.each_with_index do |name, index|
-  eval("obj#{index} = #{class_name}.new")
-  header_s.zip(content).each do |name, value|
-    eval("obj#{index}.#{name} = #{value}")
+obj = class_name.downcase
+contents.each_with_index do |content, index|
+  eval("#{obj}#{index+1} = #{class_name}.new", binding)
+  header_s.zip(content).each do |var_name, value|
+    eval("#{obj}#{index+1}.#{var_name} = #{value.inspect}", binding)
   end
-  obj_array.push(eval("obj#{index}"))
+  obj_array.push(eval("#{obj}#{index+1}", binding))
 end
