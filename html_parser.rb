9161641854d1fce5b2f7869f1844c70733d6cbaf
Taha Husain <taha.husain@weboniselab.com>
2014-08-18 15:37:55 +0530

    Finish : Assignment 6

--- html_parser.rb
+++ html_parser.rb
@@ -1,47 +1,69 @@
 #!/usr/bin/env ruby
 require "csv"
 
-str = ""
-File.open("users.html", "r") do |file|
-  file.each_line do |line|
-  	str += line
-  end
-end
+class HTMLToCSV
 
-headers = []
-result = str.scan /<th>(?<heading>[\w\s\,"]+)<\/th>/i
-result.each do |header|
-  if header[0].include? ","
-  	string = header[0].scan /[\w\s]*/i
-  	string.each do |word|
-  	  unless word.empty?
-        headers << word.strip
+  def read_html
+    @str = ""
+    @file_name = "users.html"
+    File.open(@file_name, "r") do |file|
+      file.each_line do |line|
+  	    @str += line
       end
     end
-  else
-    headers << header[0].strip
   end
-end
 
-contents = []
-content = str.scan /<td>(?<content>[[\w"\@\$\.\/,][\s:"\-\+_]]*)<\/td>/i
-content.each do |field|
-  if field[0].include? ","
-    string = field[0].scan /[\w\s\@\.\$\/_]*/i
-    string.each do |word|
-      unless word.empty?
-        contents << word.strip
+  def extract_header
+    @headers = []
+    result = @str.scan /<th>(?<heading>[\w\s\,"]+)<\/th>/i
+    result.each do |header|
+      if header[0].include? ","
+  	    string = header[0].scan /[\w\s]*/i
+  	    string.each do |word|
+  	      unless word.empty?
+            @headers << word.strip
+          end
+        end
+      else
+        @headers << header[0].strip
       end
     end
-  else
-    contents << field[0].strip
   end
-end
-contents = contents.each_slice(5).to_a
 
-CSV.open("users.csv","w") do |data|
-  data << headers
-  contents.each do |content|
-    data << content
+  def extract_contents
+    @contents = []
+    content = @str.scan /<td>(?<content>[[\w"\@\$\.\/,][\s:"\-\+_]]*)<\/td>/i
+    content.each do |field|
+      if field[0].include? ","
+        string = field[0].scan /[\w\s\@\.\$\/_]*/i
+        string.each do |word|
+          unless word.empty?
+            @contents << word.strip
+          end
+        end
+      else
+        @contents << field[0].strip
+      end
+    end
+  end
+
+  def create_csv
+    @contents = @contents.each_slice(5).to_a
+    CSV.open("#{@file_name.split(".")[0]}.csv","w") do |data|
+      data << @headers
+      @contents.each do |content|
+        data << content
+      end
+    end
+  end
+
+  def self.convert
+    parser = self.new
+    parser.read_html
+    parser.extract_header
+    parser.extract_contents
+    parser.create_csv
   end
 end
+
+HTMLToCSV.convert
--- metaprogramming.rb
+++ metaprogramming.rb
@@ -5,9 +5,8 @@ require 'mongo'
 
 binding = IRB.conf[:MAIN_CONTEXT].workspace.binding
 
-#print "Please enter csv file name: "
-#file_name = $stdin.gets.chomp
-file_name = "users.csv"
+print "Please enter csv file name: "
+file_name = $stdin.gets.chomp
 contents = CSV.read(file_name, "r")
 header = contents.shift
 header_s = []
@@ -15,38 +14,55 @@ header.each do |h|
   header_s.push(h.downcase.tr(" ", "_"))
 end
 
-def create(class_name, *args)
+def spawn_class(class_name, *args)
   instance = Class.new do
-    args.each do |arg|
-      attr_accessor arg
+    args.each do |arguement|
+      attr_accessor arguement
     end
-    define_method(:save) do |collection|
+    
+    print "Please enter the primary key of csv : "
+    p_key = $stdin.gets.chomp
+    
+    define_method(:save_object) do |collection|  
       document = Hash[self.instance_variables.collect {|name| [name, self.instance_variable_get(name)]}]
-      exists = collection.find_one("@email"=> self.instance_variable_get("@email"))
-      if exists.nil?
+      document_exists = collection.find_one("@#{p_key}"=> self.instance_variable_get("@#{p_key}"))
+      
+      if document_exists.nil?
         collection.insert(document)
       else
-        exists.update(document)
+        document_exists.update(document)
+        collection.save document_exists
       end
     end
   end
   Object.const_set class_name, instance
 end
 
-class_name = file_name.split(".")[0].capitalize.singularize
-create(class_name, *header_s)
+def create_collection(class_name)
+  mongo_client = Mongo::MongoClient.new("localhost", 27017)
 
-obj = class_name.downcase
-mongo_client = Mongo::MongoClient.new("localhost", 27017)
-db = mongo_client.db("test")
-if db.collection_names.include? class_name
-  db.drop_collection class_name
+  if mongo_client.database_names.include? "test"
+    mongo_client.drop_database "test"
+  end
+  db = mongo_client.db("test")
+  
+  if db.collection_names.include? class_name
+    db.drop_collection class_name
+  end
+  collection = db.collection(class_name)
 end
-collection = db.collection(class_name)
+
+class_name = file_name.split(".")[0].capitalize.singularize
+spawn_class(class_name, *header_s)
+obj = class_name.downcase
+collection = create_collection(class_name)
+
 contents.each_with_index do |content, index|
   eval("#{obj}#{index+1} = #{class_name}.new", binding)
+
   header_s.zip(content).each do |var_name, value|
     eval("#{obj}#{index+1}.#{var_name} = #{value.inspect}", binding)
   end
-  eval("#{obj}#{index+1}.save(collection)", binding)
+
+  eval("#{obj}#{index+1}.save_object(collection)", binding)
 end
